f21f686a8054991018efd29b1605a5ac53aab58d
Cole <coledonovan@gmail.com>
2024-12-18 12:07:25 -0600

    added both frontends to cors

--- apps/backend/src/app.js
+++ apps/backend/src/app.js
@@ -5,16 +5,21 @@ import postRoutes from './routes/postRoutes.js';
 
 const app = express();
 
+// Fetch allowed origins from environment variable or set defaults
 const allowedOrigins = process.env.ALLOWED_ORIGINS 
-  ? process.env.ALLOWED_ORIGINS.split(',') 
-  : ['http://localhost:5173', 'http://localhost:5174', 'https://colesblogproject.netlify.app/'];
+  ? process.env.ALLOWED_ORIGINS.split(',')
+  : [
+      'http://localhost:5173', 
+      'http://localhost:5174', 
+      'https://colesblogproject.netlify.app',
+      'https://colesblogprojectauthor.netlify.app'
+  ];
 
 app.use(cors({
   origin: allowedOrigins,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   credentials: true
 }));
-
 
 app.use(express.json());
 
